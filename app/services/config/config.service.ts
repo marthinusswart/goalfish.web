32632f00525017bdbc7850cdc2760e147a2c443d
Matt Swart <matt@Matts-MacBook-Pro.local>
2016-07-19 21:22:04 +1000

    soft config

---
+++ app/services/config/config.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+import { Http, Response, Headers, RequestOptions } from '@angular/http';
+import 'rxjs/add/operator/toPromise';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/catch';
+import { Observable } from 'rxjs/Rx';
+import { Subject, BehaviorSubject } from 'rxjs/Rx';
+import { Token } from '../../models/security/token';
+import { Credentials } from '../../models/security/credentials';
+
+@Injectable()
+export class ConfigService {
+    url:string = "http://localhost:3010";
+}
