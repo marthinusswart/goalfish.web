5327edc00b0691689ce6c065c68329e193bcf7e4
Matt Swart <matt@Matts-MacBook-Pro.local>
2016-08-09 22:15:45 +1000

    refactored

--- server.js
+++ server.js
@@ -5,9 +5,7 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-/*** template routes - may use later */
 var routes = require('./routes/index');
-/*** -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=- */
 
 /*
 var member = require('./routes/member/member');
@@ -26,8 +24,8 @@ var creditNote = require('./routes/creditnote/creditnote');
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+//app.set('views', path.join(__dirname, 'views'));
+//app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -35,7 +33,8 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+//app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '')));
 
 app.use('/', routes);
 
